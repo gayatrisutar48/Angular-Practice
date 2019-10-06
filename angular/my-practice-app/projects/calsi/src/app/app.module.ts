509da77c1b901afd94fd181f142a545e75dc178f
gayatrisutar48 <g.gsutar48@gmail.com>
2019-10-07 00:17:26 +0530

    calsi

--- angular/my-practice-app/projects/calsi/src/app/app.module.ts
+++ angular/my-practice-app/projects/calsi/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CalsiCompComponent } from './calsi-comp/calsi-comp.component';
 
@@ -10,7 +11,8 @@ import { CalsiCompComponent } from './calsi-comp/calsi-comp.component';
     CalsiCompComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/my-practice-app/projects/calsi/src/app/calsi-comp/calsi-comp.component.ts
+++ angular/my-practice-app/projects/calsi/src/app/calsi-comp/calsi-comp.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalsiCompComponent implements OnInit {
 
+  public num1
+  public num2
+  public result
   constructor() { }
 
   ngOnInit() {
   }
 
+  addition() {
+    this.result = this.num1 + this.num2
+  }
+  subtraction() {
+    this.result = this.num1 - this.num2
+  }
+  multiplication() {
+    this.result = this.num1 * this.num2
+  }
+  division() {
+    this.result = this.num1 / this.num2
+  }
 }
