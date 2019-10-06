740cdaafe5900280ae5c9b615594148bad4ad6f5
gayatrisutar48 <g.gsutar48@gmail.com>
2019-10-06 18:18:16 +0530

    country flag

---
+++ angular/my-practice-app/projects/country-flag/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { CountryListComponent } from './country-list/country-list.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ParentComponent,
+    CountryListComponent,
+    BodyComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
